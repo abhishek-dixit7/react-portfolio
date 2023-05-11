179d45703fdb21cab4c06070f2bb6bdfa6f1c904
Abhishek <abhishekdixit9024@gmail.com>
2023-05-11 15:02:02 +0530

    Updated styles and added links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Menu = () => (
 
 const Navbar = () => {
   return (
-    <div className="portfolio__navbar">
+    <div className="portfolio__navbar section__padding">
       <div className="portfolio__navbar-links">
         <div className="portfolio__navbar-links_logo">
           <img src={logo} alt="logo" />
--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import "./home.css";
 import profile from "../../assets/profile.jpg";
+import { SlSocialTwitter, SlSocialInstagram } from "react-icons/sl";
+import { SiGithub, SiLinkedin } from "react-icons/si";
 
 const Home = () => {
   return (
     <div className="portfolio__home section__padding">
       <div className="portfolio__home-image">
         <img src={profile} alt="profile" />
-        <h1>Abhishek Dixit</h1>
       </div>
       <div className="portfolio__home-content">
+        <h1>Abhishek Dixit</h1>
+        <h4>Software Developer</h4>
         <ul>
           <li>Developer</li>
           <li>JS, C#, React and Node</li>
           <li>Gamer</li>
           <li>Quick Learner</li>
         </ul>
+        <div className="portfolio__home-content-links">
+          <a href="https://twitter.com/your_username">
+            <SlSocialTwitter />
+          </a>
+          <a href="https://twitter.com/your_username">
+            <SlSocialInstagram />
+          </a>
+          <a href="https://twitter.com/your_username">
+            <SiGithub />
+          </a>
+          <a href="https://twitter.com/your_username">
+            <SiLinkedin />
+          </a>
+        </div>
       </div>
     </div>
   );
