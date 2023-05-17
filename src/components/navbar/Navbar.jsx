577309c18020e40e541e15647648c14198e287d9
Abhishek <abhishekdixit9024@gmail.com>
2023-05-17 10:34:04 +0530

    added and updated media queries!

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import { React, useState } from "react";
 import "./navbar.css";
 import logo from "../../assets/logo.jpg";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 
 const Menu = () => (
   <>
@@ -8,18 +9,20 @@ const Menu = () => (
       <a href="#home">Home</a>
     </p>
     <p>
-      <a href="#about">AboutMe</a>
+      <a href="#about">About</a>
     </p>
     <p>
       <a href="#project">Projects</a>
     </p>
     <p>
-      <a href="#contact">ContactMe</a>
+      <a href="#contact">Contact</a>
     </p>
   </>
 );
 
 const Navbar = () => {
+  const [toggleMenu, settoggleMenu] = useState(false);
+
   return (
     <div className="portfolio__navbar section__padding">
       <div className="portfolio__navbar-links">
@@ -28,6 +31,28 @@ const Navbar = () => {
         </div>
         <div className="portfolio__navbar-links_container">
           <Menu />
+        </div>
+        <div className="portfolio__navbar-menu">
+          {toggleMenu ? (
+            <RiCloseLine
+              color="#fff"
+              size={27}
+              onClick={() => settoggleMenu(false)}
+            />
+          ) : (
+            <RiMenu3Line
+              color="#fff"
+              size={27}
+              onClick={() => settoggleMenu(true)}
+            />
+          )}
+          {toggleMenu && (
+            <div className="portfolio__navbar-menu_container scale-up-center">
+              <div className="portfolio__navbar-menu_container-links">
+                <Menu />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
